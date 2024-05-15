0347aed206d27b78e6c5650bd27644fca821790d
Flemming Knøss Hansen <fha@kmd.dk>
2024-05-15 15:23:11 +0200

    Remove strict mode since I caused problems with weak generic

--- lib/wcshadow.d.ts
+++ lib/wcshadow.d.ts
@@ -5,6 +5,19 @@ export declare class ShadowElement extends HTMLElement {
     static defaultOptions: ShadowRootInit;
     shadow: ShadowRoot;
     constructor(options?: ShadowRootInit);
+    /**
+     * Find element in shadowDom
+     * Don't use in attributeChangedCallback or before dom has been written
+     * @param query html query to find element in this.shadow
+     */
+    shadowSelector<T extends HTMLElement>(query: string): T;
+    /**
+     * Fetches a HTML template as a string.
+     * NOTE! You cannot use scripts.
+     * @param {string} src Source of the html template asset
+     * @returns {string} html as string
+     */
+    getTemplate(src: string): Promise<string>;
 }
 /**
  * define helps defining custom components:
