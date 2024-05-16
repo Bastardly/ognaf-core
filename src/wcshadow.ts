73e0cb0169bf831a855d016fff7d3f59b2dc3e45
Flemming Knøss Hansen <fha@kmd.dk>
2024-05-16 13:24:10 +0200

    Bump to version 0.1.7

--- src/wcshadow.ts
+++ src/wcshadow.ts
@@ -8,20 +8,25 @@ export class ShadowElement extends HTMLElement {
 
   shadow: ShadowRoot;
 
+  /**
+   * Constructor takes the same options as when creating a custom component with a shadow dom. 
+   * It defaults to { mode: 'open' }
+   * @param {ShadowRootInit} options 
+   */
   constructor(options?: ShadowRootInit) {
     super();
     this.shadow = this.attachShadow(options || ShadowElement.defaultOptions);
   }
 
   	/**
-	 * Find element in shadowDom
+	 * Find element in shadowDom, assuming it exists by typecasting generic. 
+   * This is not best practice, but useful when you don't want clog small scope components.
 	 * Don't use in attributeChangedCallback or before dom has been written
 	 * @param query html query to find element in this.shadow
 	 */
 	shadowSelector<T extends HTMLElement>(query: string) {
 		return this.shadow.querySelector(query) as T;
 	}
-
 }
 
 /**
