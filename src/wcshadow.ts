4715d2450e57a83d2a3f398f2bd6c645b4efcd5e
Flemming Knøss Hansen <fha@kmd.dk>
2024-05-24 08:49:20 +0200

    Clean up the JSDoc a bit

--- src/wcshadow.ts
+++ src/wcshadow.ts
@@ -1,9 +1,7 @@
 /**
  * ShadowElement extends HTMLElement and attaches shadowRoot as shadow.
  * 
- * It has the following methods:
- * 
- * More info on {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#using_the_lifecycle_callbacks|MDN}
+ *  {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#using_the_lifecycle_callbacks|It has the following methods:}
  * 
  *  @example
  * connectedCallback()
