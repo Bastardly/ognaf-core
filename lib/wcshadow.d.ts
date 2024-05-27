f4944aa8eb73562739601a7b3934b6f5268ff492
Flemming Knøss Hansen <fha@kmd.dk>
2024-05-27 11:54:43 +0200

    Bump to 0.1.10

--- lib/wcshadow.d.ts
+++ lib/wcshadow.d.ts
@@ -1,5 +1,23 @@
 /**
- * ShadowElement extends HTMLElement and attaches shadowRoot and applies a couple of utility methods.
+ * ShadowElement extends HTMLElement and attaches shadowRoot as shadow.
+ *
+ *  {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#using_the_lifecycle_callbacks|It has the following methods:}
+ *
+ *  @example
+ * connectedCallback()
+ * Invoked when the ShadowElement is first connected to the DOM.
+ *
+ * disconnectedCallback()
+ * Invoked when the ShadowElement is disconnected from the DOM.
+ *
+ * adoptedCallback()
+ * Invoked when the ShadowElement is moved to a new document.
+ *
+ * attributeChangedCallback(name, oldValue, newValue)
+ * Invoked when one of the ShadowElement's attributes is added, removed, or changed.
+ * When using attributeChangedCallback, remember to set observedAttributes as static.
+ * E.g. static observedAttributes = ["guid", "variant"];
+ * Otherwise attribute changes will be ignored.
  */
 export declare class ShadowElement extends HTMLElement {
     static defaultOptions: ShadowRootInit;
@@ -15,5 +33,21 @@ export declare class ShadowElement extends HTMLElement {
  * define helps defining custom components:
  * define('custom-name', class extends ShadowElement { ... })
  * @returns {string} name used to define custom component
+ * @example
+ * import { define } from "@ognaf/core";
+
+ * define("hello-world", class extends ShadowElement {
+    constructor() {
+        super();
+        this.shadow.innerHTML = "<div>Hello world!</div>";
+    }
+})
+ *
+ * The define helper only define the custom element if it does not already exist.
+ * Otherwise, it will be ignored. Once the component is defined, we can access it anywhere
+ * in the DOM by its given name.
+ *
+ * @example
+ * <my-component></my-component>
  */
-export declare function define(name: string, webcomponentClass: CustomElementConstructor): string;
+export declare function define(name: string, webcomponentClass: CustomElementConstructor, options?: ElementDefinitionOptions): string;
